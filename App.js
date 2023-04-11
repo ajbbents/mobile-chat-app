c7c717fa74cf1e5fd7e69c52dbaa445a1839f9f9
ajbbents <107074385+ajbbents@users.noreply.github.com>
2023-04-11 10:36:30 -0500

    Add firestore

--- App.js
+++ App.js
@@ -7,6 +7,26 @@ import Chat from './components/Chat';
 
 const Stack = createNativeStackNavigator();
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDee2HVReF4Btlz-4VzWW7EOKTffEj2JwU",
+  authDomain: "mobilechatapp-5cc6d.firebaseapp.com",
+  projectId: "mobilechatapp-5cc6d",
+  storageBucket: "mobilechatapp-5cc6d.appspot.com",
+  messagingSenderId: "681227909795",
+  appId: "1:681227909795:web:3a81a356242e0c511a1d16"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Cloud Firestore and get a reference to service
+const db = getFirestore(app);
+
 // construct the app
 export default class App extends React.Component {
   render() {
