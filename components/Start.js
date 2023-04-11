1a20e70e41268739c6d3701672586931b2fb72a0
ajbbents <107074385+ajbbents@users.noreply.github.com>
2023-04-11 13:39:24 -0500

    adjust color picker

--- components/Start.js
+++ components/Start.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import { ImageBackground, StyleSheet, View, Text, TextInput, TouchableOpacity } from "react-native";
+import React, { useState } from 'react';
+import { ImageBackground, StyleSheet, View, Text, TextInput, TouchableOpacity, Alert, Image } from "react-native";
+import { getAuth, signInAnonymously } from "firebase/auth";
 
 const backgroundColors = {
   black: { backgroundColor: '#090c08' },
@@ -8,61 +9,78 @@ const backgroundColors = {
   green: { backgroundColor: '#B9C6AE' }
 };
 
-export default class Start extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { name: '', color: '' }
-  }
-  render() {
-    const { black, purple, grey, green } = backgroundColors
-    return (
-      <View style={styles.container}>
-        <ImageBackground source={require('../assets/background-image.png')} style={styles.image}>
-          <Text style={styles.title}>Hello from start</Text>
-          {/* user input portion of page */}
-          <View style={styles.inputBox}>
-            {/* user name box */}
-            <TextInput
-              style={styles.nameInput}
-              value={this.state.name}
-              onChangeText={(name) => this.setState({ name })}
-              placeholder='Your Name'
-            />
-            {/* background color chooser */}
-            <View>
-              <Text style={styles.colorText}>Choose your background:</Text>
-              <View style={styles.colorWrapper}>
-                <TouchableOpacity
-                  style={[styles.color, black]}
-                  onPress={() => this.setState({ color: black.backgroundColor })}
-                />
-                <TouchableOpacity
-                  style={[styles.color, purple]}
-                  onPress={() => this.setState({ color: purple.backgroundColor })}
-                />
-                <TouchableOpacity
-                  style={[styles.color, grey]}
-                  onPress={() => this.setState({ color: grey.backgroundColor })}
-                />
-                <TouchableOpacity
-                  style={[styles.color, green]}
-                  onPress={() => this.setState({ color: green.backgroundColor })}
-                />
-              </View>
+const SelectedColorOverlay = () => <View style={styles.selectedColorOverlay} />;
+
+const Start = ({ navigation }) => {
+  //declare state variables
+  const [name, setName] = useState("");
+  const [color, setColor] = useState("");
+
+  //firebase auth anonymous signin
+  const handleSignIn = () => {
+    const auth = getAuth();
+    signInAnonymously(auth)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        navigation.navigate("Chat", {
+          name,
+          color,
+          userID: user.uid,
+        });
+        Alert.alert("you are now signed in");
+      })
+      .catch((error) => {
+        Alert.alert("not able to sign in, try back later");
+      });
+  };
+
+  // background color option mapping
+  const { black, purple, grey, green } = backgroundColors;
+  const colorOptions = Object.entries(backgroundColors).map(([key, value]) => (
+    <TouchableOpacity
+      key={key}
+      style={[
+        styles.color,
+        value,
+        color === value.backgroundColor && styles.colorSelected,
+      ]}
+      onPress={() => setColor(value.backgroundColor)}
+    >
+      {color === value.backgroundColor && <SelectedColorOverlay />}
+    </TouchableOpacity>
+  ));
+
+  return (
+    <View style={styles.container}>
+      <ImageBackground source={require('../assets/background-image.png')} style={styles.image}>
+        <Text style={styles.title}>let's chat</Text>
+        {/* user input portion of page */}
+        <View style={styles.inputBox}>
+          {/* user name box */}
+          <TextInput
+            style={styles.nameInput}
+            value={name}
+            onChangeText={setName}
+            placeholder='Your Name'
+          />
+          {/* background color chooser */}
+          <View>
+            <Text style={styles.colorText}>Choose your background:</Text>
+            <View style={styles.colorWrapper}>
+              <View style={styles.colorOptions}>{colorOptions}</View>
             </View>
-            {/* enter chat button */}
-            <TouchableOpacity
-              style={[styles.chatButton]}
-              onPress={() => this.props.navigation.navigate('Chat',
-                { name: this.state.name, color: this.state.color })}>
-              <Text style={styles.chatButtonText}>Start Chatting</Text>
-            </TouchableOpacity>
-            {Platform.OS === "ios" ? <KeyboardAvoidingView behavior="padding" /> : null}
           </View>
-        </ImageBackground>
-      </View>
-    );
-  }
+          {/* enter chat button */}
+          <TouchableOpacity
+            style={[styles.chatButton]}
+            onPress={handleSignIn}>
+            <Text style={styles.chatButtonText}>Start Chatting</Text>
+          </TouchableOpacity>
+          {Platform.OS === "ios" ? <KeyboardAvoidingView behavior="padding" /> : null}
+        </View>
+      </ImageBackground>
+    </View>
+  );
 }
 
 // Styling
@@ -89,6 +107,13 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     marginBottom: 20,
     paddingVertical: 20,
+  },
+  inputImage: {
+    width: 24,
+    height: 24,
+    resizeMode: "contain",
+    marginRight: 10,
+    opacity: 0.6,
   },
   nameInput: {
     height: 50,
@@ -120,6 +145,22 @@ const styles = StyleSheet.create({
     borderRadius: 22,
     margin: 10,
   },
+  colorOptions: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 10,
+  },
+  selectedColorOverlay: {
+    position: "absolute",
+    width: 48,
+    height: 48,
+    borderRadius: 24,
+    backgroundColor: "transparent",
+    justifyContent: "center",
+    alignItems: "center",
+    top: -4,
+    bottom: -4,
+  },
   chatButton: {
     height: 50,
     width: '88%',
@@ -136,3 +177,6 @@ const styles = StyleSheet.create({
     fontSize: 16,
   },
 });
+export default Start;
+
+//() => this.props.navigation.navigate('Chat', { name: this.state.name, color: this.state.color })
