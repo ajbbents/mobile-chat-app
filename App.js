c34155fb606bbfd751b5bcec22bb04d984b1dcb9
ajbbents <107074385+ajbbents@users.noreply.github.com>
2023-04-11 11:34:57 -0500

    add firebase and firestore

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 // import navigation screens
 import Start from './components/Start';
 import Chat from './components/Chat';
+import { useEffect } from 'react';
 
 const Stack = createNativeStackNavigator();
 
@@ -12,39 +13,45 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDee2HVReF4Btlz-4VzWW7EOKTffEj2JwU",
-  authDomain: "mobilechatapp-5cc6d.firebaseapp.com",
-  projectId: "mobilechatapp-5cc6d",
-  storageBucket: "mobilechatapp-5cc6d.appspot.com",
-  messagingSenderId: "681227909795",
-  appId: "1:681227909795:web:3a81a356242e0c511a1d16"
-};
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
 
-// Initialize Cloud Firestore and get a reference to service
-const db = getFirestore(app);
+
 
 // construct the app
-export default class App extends React.Component {
-  render() {
-    return (
-      <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName='Start'
-        >
-          <Stack.Screen
-            name="Start"
-            component={Start}
+const App = () => {
+  const firebaseConfig = {
+    apiKey: "AIzaSyDee2HVReF4Btlz-4VzWW7EOKTffEj2JwU",
+    authDomain: "mobilechatapp-5cc6d.firebaseapp.com",
+    projectId: "mobilechatapp-5cc6d",
+    storageBucket: "mobilechatapp-5cc6d.appspot.com",
+    messagingSenderId: "681227909795",
+    appId: "1:681227909795:web:3a81a356242e0c511a1d16"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  // Initialize Cloud Firestore and get a reference to service
+  const db = getFirestore(app);
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName='Start'
+      >
+        <Stack.Screen
+          name="Start"
+          component={Start}
+        />
+        <Stack.Screen name="Chat">
+          <Chat
+            db={db}
+            {...props}
           />
-          <Stack.Screen
-            name="Chat"
-            component={Chat}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-    );
-  }
-}
+        </Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
